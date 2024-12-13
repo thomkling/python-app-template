5bcd321fd2a067940587c34e8f87b5b6324a6c62
Thomas Kling <thomas.kling@banno.com>
2024-12-13 17:43:01 -0600

    Move test routes out of the app and into the tests

--- app/router.py
+++ app/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 import structlog
 import uuid
 
@@ -9,13 +9,3 @@ log = structlog.stdlib.get_logger(f"app_logs")
 @router.get("/ping")
 async def ping():
     return "PONG"
-
-@router.get("/exception")
-async def exception():
-    raise HTTPException(status_code=500, detail="exception yo!")
-
-@router.get("/widget/{widget_id}")
-async def get_widget(widget_id: uuid.UUID):
-    structlog.contextvars.bind_contextvars(widget_id=widget_id)
-    log.info("Successfully retrieved widget")
-    return {"widget_id": widget_id}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,32 @@
 from app.main import app
 from fastapi.testclient import TestClient
+from fastapi import HTTPException
+import uuid
+
+@app.get("/exception")
+async def exception():
+    raise HTTPException(status_code=500, detail="exception yo!")
+
+@app.get("/widget/{widget_id}")
+async def get_widget(widget_id: uuid.UUID):
+    return {"widget_id": widget_id}
 
 client = TestClient(app)
 
+# ping should be available on all apps using the template
 def test_ping():
     response = client.get("/ping")
     assert response.status_code == 200
-    # fastapi returns a quoted string in the response
-    assert response.text == '"PONG"'
+    assert response.json() == "PONG"
+
+# testing that HTTPException raised in the route is handled outside the middleware exception catcher
+def test_exception():
+    response = client.get("/exception")
+    assert response.status_code == 500
+    assert response.json() == {"detail":"exception yo!"}
+
+# happy path test
+def test_widget():
+    response = client.get("/widget/7c3853a3-864a-4513-ac97-c3d6fd3776c9")
+    assert response.status_code == 200
+    assert response.json() == {"widget_id":"7c3853a3-864a-4513-ac97-c3d6fd3776c9"}
